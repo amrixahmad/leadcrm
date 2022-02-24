499301428458b02edb52b639757dd49d04ba544c
Amri Ahmad <amri.ahmad85@gmail.com>
2022-02-24 22:00:28 +0800

    debug user auth

--- frontend/leadcrm/src/components/RegisterUser.js
+++ frontend/leadcrm/src/components/RegisterUser.js
@@ -42,7 +42,7 @@ const RegisterUser = () => {
 
             <div className="section-title">
                 <h2>Register</h2>
-                <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+                <p>You know you want to.</p>
             </div>
         
             <div className="row mt-5">
